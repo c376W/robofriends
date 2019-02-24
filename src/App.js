1771f5c1caef3e3e024decec9a620bec9e0e5fe4
c376W <mailfzc123@gmail.com>
2019-02-24 11:26:01 -0800

    finishing up with the searchbox

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import CardList from './CardList';
 import SearchBox from './SearchBox'
 import {robots} from './robots';
 
-
 class App extends Component{
 	constructor(){
-		super();
+		super()
 		this.state = {
 			robots: robots,
 			searchfield: ''
 		}
 	}
-	onSearchChange(event){
-		console.log(event);
+	onSearchChange=(event)=>{
+		this.setState({searchfield:event.target.value})
 	}
+
 	render(){
+		const filteredRobots = this.state.robots.filter(robots=>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
 		return (
 			<div className='tc'>
 				<h1>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={this.state.robots}/>
+				<CardList robots={filteredRobots}/>
 			</div>
 		);
 	}
