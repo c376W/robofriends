9af1ba4d2f2125cf243edadeac6b4c08c2bae06e
c376W <mailfzc123@gmail.com>
2019-02-27 12:25:55 -0800

    Adding scrollings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
+import Scroll from './Scroll';
 // import {robots} from './robots';
 import './App.css';
 
@@ -38,7 +39,9 @@ class App extends Component{
 				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
 					<SearchBox searchChange={this.onSearchChange}/>
-					<CardList robots={filteredRobots}/>
+					<Scroll>
+						<CardList robots={filteredRobots}/>
+					</Scroll>
 				</div>
 			);
 		}
