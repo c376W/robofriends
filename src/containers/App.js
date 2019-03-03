3f48f287bd88561a8f1c5785062541bf8e06386c
c376W <mailfzc123@gmail.com>
2019-03-02 17:16:32 -0800

    Adding files and changing syntax.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox'
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 // import {robots} from './robots';
 import './App.css';
 
@@ -28,14 +28,14 @@ class App extends Component{
 	}
 
 	render(){
-		const filteredRobots = this.state.robots.filter(robots=>{
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot=>{
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
-		if(this.state.robots.length===0){
-			return <h1>Loading</h1>
-		}else{
+		  return !robots.length ?
+			<h1>Loading</h1> :
 
-			return (
+			(
 				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
 					<SearchBox searchChange={this.onSearchChange}/>
@@ -44,8 +44,8 @@ class App extends Component{
 					</Scroll>
 				</div>
 			);
-		}
-	}
+	}	
+	
 }
 
 export default App;
