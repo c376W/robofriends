7dc505ab958a1806ea029c63db1bc78821c865e5
c376W <mailfzc123@gmail.com>
2019-06-23 10:27:36 -0700

    Updates

---
+++ precache-manifest.f42c97d82976af4a187f93113d20a3ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "0a2a9df22d1107184115",
+    "url": "/robofriends/static/js/main.0a2a9df2.chunk.js"
+  },
+  {
+    "revision": "99aa622b1ae25df2c04b",
+    "url": "/robofriends/static/js/2.99aa622b.chunk.js"
+  },
+  {
+    "revision": "0a2a9df22d1107184115",
+    "url": "/robofriends/static/css/main.1fc0840b.chunk.css"
+  },
+  {
+    "revision": "99aa622b1ae25df2c04b",
+    "url": "/robofriends/static/css/2.fd976d19.chunk.css"
+  },
+  {
+    "revision": "f4d1ed76f0c3f498615ab83e42fcab66",
+    "url": "/robofriends/index.html"
+  }
+];
